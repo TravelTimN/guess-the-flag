531277ec6b8342b26a7ef5e75383034103df7382
Tim Nelson <traveltimn@gmail.com>
2023-12-21 17:14:28 +0000

    Add 2 countries per region for testing

---
+++ assets/js/countries.js
@@ -0,0 +1,125 @@
+/* jshint esversion: 11 */
+
+const countries = [
+    {
+        "name": "Afghanistan",
+        "iso": "af",
+        "region": "Asia",
+        "options": [
+            {"name": "Afghanistan", "iso": "af"},
+            {"name": "Malawi", "iso": "mw"},
+            {"name": "United Arab Emirates", "iso": "ae"},
+            {"name": "Libya", "iso": "ly"},
+        ]
+    },
+    {
+        "name": "Åland Islands",
+        "iso": "ax",
+        "region": "Europe",
+        "options": [
+            {"name": "Åland Islands", "iso": "ax"},
+            {"name": "Sweden", "iso": "se"},
+            {"name": "Iceland", "iso": "is"},
+            {"name": "Faroe Islands", "iso": "fo"},
+        ]
+    },
+    {
+        "name": "Albania",
+        "iso": "al",
+        "region": "Europe",
+        "options": [
+            {"name": "Albania", "iso": "al"},
+            {"name": "Montenegro", "iso": "me"},
+            {"name": "Isle of Man", "iso": "im"},
+            {"name": "Hong Kong", "iso": "hk"},
+        ]
+    },
+    {
+        "name": "Algeria",
+        "iso": "dz",
+        "region": "Africa",
+        "options": [
+            {"name": "Algeria", "iso": "dz"},
+            {"name": "Nigeria", "iso": "ng"},
+            {"name": "Norfolk Island", "iso": "nf"},
+            {"name": "Pakistan", "iso": "pk"},
+        ]
+    },
+    {
+        "name": "Angola",
+        "iso": "ao",
+        "region": "Africa",
+        "options": [
+            {"name": "Angola", "iso": "ao"},
+            {"name": "Germany", "iso": "de"},
+            {"name": "Belgium", "iso": "be"},
+            {"name": "Uganda", "iso": "ug"},
+        ]
+    },
+    {
+        "name": "Anguilla",
+        "iso": "ai",
+        "region": "Americas",
+        "options": [
+            {"name": "Anguilla", "iso": "ai"},
+            {"name": "Montserrat", "iso": "ms"},
+            {"name": "Turks and Caicos", "iso": "tc"},
+            {"name": "Cayman Islands", "iso": "ky"},
+        ]
+    },
+    {
+        "name": "Antigua and Barbuda",
+        "iso": "ag",
+        "region": "Americas",
+        "options": [
+            {"name": "Antigua and Barbuda", "iso": "ag"},
+            {"name": "Timor-Leste", "iso": "tl"},
+            {"name": "Trinidad and Tobago", "iso": "tt"},
+            {"name": "Bermuda", "iso": "bm"},
+        ]
+    },
+    {
+        "name": "Armenia",
+        "iso": "am",
+        "region": "Asia",
+        "options": [
+            {"name": "Armenia", "iso": "am"},
+            {"name": "Colombia", "iso": "co"},
+            {"name": "Venezuela", "iso": "ve"},
+            {"name": "Ecuador", "iso": "ec"},
+        ]
+    },
+    {
+        "name": "American Samoa",
+        "iso": "as",
+        "region": "Oceania",
+        "options": [
+            {"name": "American Samoa", "iso": "as"},
+            {"name": "Samoa", "iso": "ws"},
+            {"name": "Liberia", "iso": "lr"},
+            {"name": "Antigua and Barbuda", "iso": "ag"},
+        ]
+    },
+    {
+        "name": "Antarctica",
+        "iso": "aq",
+        "region": "Oceania",
+        "options": [
+            {"name": "Antarctica", "iso": "aq"},
+            {"name": "Micronesia", "iso": "fm"},
+            {"name": "Somalia", "iso": "so"},
+            {"name": "Saint Lucia", "iso": "lc"},
+        ]
+    },
+    // {
+    //     "name": "",
+    //     "iso": "",
+    //     "region": "",
+    //     "options": [
+    //         {"name": "", "iso": ""},
+    //         {"name": "", "iso": ""},
+    //         {"name": "", "iso": ""},
+    //         {"name": "", "iso": ""},
+    //     ]
+    // },
+];
